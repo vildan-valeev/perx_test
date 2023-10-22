30d58dec65bf0ff3a97e3b0714d038b9708ea98d
Вильдан Валеев <vildan.maydanovich.valeev@gmail.com>
2023-10-22 05:08:27 +0500

    воркерпул

---
+++ app/pkg/pool/pool.go
@@ -0,0 +1,57 @@
+package pool
+
+import (
+	"log"
+	"sync"
+)
+
+// Pool is the worker pool
+type Pool struct {
+	Workers []*Worker
+
+	concurrency   int
+	collector     chan *Task
+	runBackground chan bool
+	wg            sync.WaitGroup
+}
+
+// NewPool initializes a new pool with the given tasks and
+// at the given concurrency.
+func NewPool(concurrency int) *Pool {
+	if concurrency < 0 || concurrency > 100 {
+		concurrency = 4
+	}
+
+	return &Pool{
+		concurrency: concurrency,
+		collector:   make(chan *Task, 1000),
+	}
+}
+
+// AddTask adds a task to the pool
+func (p *Pool) AddTask(task *Task) {
+	p.collector <- task
+}
+
+// Run runs all work within the pool and blocks until it's
+// finished.
+func (p *Pool) Run() {
+	log.Printf("Starting Worker Pool with N=%d workers", p.concurrency)
+	for i := 1; i <= p.concurrency; i++ {
+		worker := NewWorker(p.collector, i)
+		p.Workers = append(p.Workers, worker)
+		go worker.Start()
+	}
+
+	p.runBackground = make(chan bool)
+	<-p.runBackground
+}
+
+// Stop stops background workers
+func (p *Pool) Stop() {
+	log.Println("Stopping Worker Pool")
+	for i := range p.Workers {
+		p.Workers[i].Stop()
+	}
+	p.runBackground <- true
+}
---
+++ app/pkg/pool/task.go
@@ -0,0 +1,20 @@
+package pool
+
+import (
+	"log"
+)
+
+type Task struct {
+	Err  error
+	Data interface{}
+	f    func(interface{}) error
+}
+
+func NewTask(f func(interface{}) error, data interface{}) *Task {
+	return &Task{f: f, Data: data}
+}
+
+func process(workerID int, task *Task) {
+	log.Printf("Worker %d processes task %v\n", workerID, task.Data)
+	task.Err = task.f(task.Data)
+}
